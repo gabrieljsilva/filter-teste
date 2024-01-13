65e7f22f7de36bebaa1c46d1b012b36331a08bec
Gabriel Silva <gabrieljsilva2301@gmail.com>
2024-01-13 15:59:31 -0300

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestFilterModule } from './third-party/nest-filters/module';
-import { ToPrismaQueryPipe } from './pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
