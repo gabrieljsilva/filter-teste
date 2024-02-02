e69b76b729ca4e3b21d5c967d811fa10a680f5f0
Gabriel Silva <gabrieljsilva2301@gmail.com>
2024-02-02 20:04:24 -0300

    added: base filter

--- src/third-party/nest-filters/index.ts
+++ src/third-party/nest-filters/index.ts
@@ -1,7 +1,7 @@
 export { FilterableField } from './decorators/filterable-field.decorator';
 export { FilterableEntity } from './decorators/filterable-entity.decorator';
 export { FilterArgs } from './decorators/filter-args.decorator';
+export { getFilterOf } from './utils/get-filter-of';
 
 export { FilterOf } from './types/filter-of.type';
 export { COMPARISON_OPERATOR } from './types/comparison-operators';
-export { getFilterOf } from './utils/get-filter-of';
